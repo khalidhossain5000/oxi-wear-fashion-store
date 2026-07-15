88eb36ba74bc9516c034697f0d64864e4cab3ec7
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-07-15 12:09:49 +0600

    theme setup and dark mode setup completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Playfair_Display, Sora } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/providers/ThemeProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const playfair = Playfair_Display({
+  variable: "--font-playfair-display",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const sora = Sora({
+  variable: "--font-sora",
   subsets: ["latin"],
 });
 
@@ -20,9 +21,19 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
+      className={`${playfair.variable} ${sora.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider 
+         attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+        {children}
+        </ThemeProvider>
+        </body>
     </html>
   );
 }
